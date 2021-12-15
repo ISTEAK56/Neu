385e7de066509f622c8e641abf6ce129db87f49f
ISTEAK56 <79440136+ISTEAK56@users.noreply.github.com>
2021-12-15 09:46:26 +0530

    Add files via upload

---
+++ motherfuckr.js
@@ -0,0 +1,8 @@
+var Denmark = getElementById.something ;
+Denmark =["AgileData","Fragiledata","keen","neat ","absulate","woozy","dizzy","pentagon","sour","bitty","monster","bug","cricket","soccer","chipsing","cuvert"]
+for (lol=0; lol<20; lol++){
+    return Denmark()
+}
+
+
+
